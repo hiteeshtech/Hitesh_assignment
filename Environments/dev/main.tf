ada6a709e8d6a79ae14fbc2afe1a58b3efa0bb15
hiteeshtech <hiteesh.tech@gmail.com>
2025-12-19 23:26:45 +0530

    Add repository files

---
+++ Environments/dev/main.tf
@@ -0,0 +1,36 @@
+module "resource_group" {
+  source  = "../../modules/azurerm_resource_group"
+  rg_name = var.rg_name
+}
+
+module "virtual_network" {
+  source    = "../../modules/azurerm_networking"
+  vnet_name = var.vnet_name
+}
+
+module "key_vaults" {
+  source  = "../../modules/azurerm_key_vaults"
+  kv_name = var.key_vaults
+}
+
+module "public_ip" {
+  source         = "../../modules/azurerm_public_ip"
+  public_ip_name = var.public_ip_name
+}
+
+module "virtual_machine" {
+  source = "../../modules/azurerm_compute"
+  vms    = var.vms
+
+}
+
+module "mssql_server" {
+  source            = "../../modules/azurerm_sql_server"
+  mssql_server_name = var.mssql_server_name
+}
+
+module "mssql_database" {
+  source          = "../../modules/azurerm_sql_database"
+  mssql_db_name   = var.mssql_db_name
+  mssql_server_id = module.mssql_server.mssql_server_id["sql_server_1"]
+}
